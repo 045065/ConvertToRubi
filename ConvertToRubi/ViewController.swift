92da9b8f9c984080a192ddd42b4530c80e11732f
py on <takuya.ya.e.ii.0mj@gmail.com>
2019-07-28 17:33:48 +0900

    Initial Commit

---
+++ ConvertToRubi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConvertToRubi
+//
+//  Created by 藤枝拓弥 on 2019/07/28.
+//  Copyright © 2019 藤枝拓弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
